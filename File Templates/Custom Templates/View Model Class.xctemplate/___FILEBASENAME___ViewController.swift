467d2196fc3a86beed766a18610226382e5c8d6f
Andrej Jasso <andrej.jasso@goodrequest.com>
2020-06-24 20:05:52 +0200

    no message

---
+++ File Templates/Custom Templates/View Model Class.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,9 @@
+//___FILEHEADER___
+
+import UIKit
+
+
+class ___FILEBASENAMEASIDENTIFIER___:  UIViewController {
+
+
+}
---
+++ File Templates/Custom Templates/View Model Class.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,25 @@
+//___FILEHEADER___
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___Delegate {
+
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___Protocol {
+
+}
+
+class ___FILEBASENAMEASIDENTIFIER___:  ___FILEBASENAMEASIDENTIFIER___Protocol {
+
+    // MARK: - Delegate
+
+    weak var delegate:  ___FILEBASENAMEASIDENTIFIER___Delegate
+
+    // MARK: - Initializer
+
+    init() {
+
+    }
+
+}
