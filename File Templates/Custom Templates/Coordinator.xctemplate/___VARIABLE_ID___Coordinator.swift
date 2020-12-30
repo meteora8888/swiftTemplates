d17a6ff767fb7d6958988f2716e323dd3b5ae0d6
Andrej Jasso <andrej.jasso@goodrequest.com>
2020-12-30 14:13:19 +0100

    Improved coordinator template

--- File Templates/Custom Templates/Coordinator.xctemplate/___VARIABLE_ID___Coordinator.swift
+++ File Templates/Custom Templates/Coordinator.xctemplate/___VARIABLE_ID___Coordinator.swift
@@ -2,45 +2,66 @@
 
 import UIKit
 
+// MARK: - Steps
+
+enum ___VARIABLE_ID___Step {
+
+}
+
 // MARK: - View Model Implementation
 
-final class ___VARIABLE_ID___Coordinator: Coordinator<AppStepper> {
+final class ___VARIABLE_ID___Coordinator: Coordinator<AppStep> {
 
-    // MARK: - Class definitions
+    // MARK: - Typealias
 
-    typealias DI = di
-
-    struct Config {
-        let navigationController: UINavigationController
-        let di: DI
-    }
+    typealias DI = DependencyContainer
 
     // MARK: - Constant
 
-    // MARK: - Variable
-
-    internal var config: Config
+    private let di: DI
+    private let parentCoordinator: Coordinator<AppStep>
 
     // MARK: - Initializer
 
-    init(configuration: Config) {
-        self.config = configuration
-
-        super.init(navigationController: config.navigationController)
+    init(di: DependencyContainer,
+         parentCoordinator: Coordinator<AppStep>,
+         navigationController: UINavigationController?) {
+        self.di = di
+        self.parentCoordinator = parentCoordinator
+        super.init(navigationController: navigationController ?? SFZNavigationController.create())
     }
 
-    override func navigate(to step: AppStepper) -> Flow {
+    // MARK: - Public
+
+    override func navigate(to step: AppStep) -> StepAction {
         switch step {
         default: return .none
         }
     }
 
-    func reset() {
-        config.navigationController.popToRootViewController(animated: true)
+    // MARK: - Override
+
+    override func start() -> UIViewController? {
+        super.start()
+
+        let ___VARIABLE_ID___Controller = ___VARIABLE_ID___ViewController.create(
+            viewModel: ___VARIABLE_ID___ViewModel(
+                di: di,
+                coordinator: self
+            )
+        )
+
+        return ___VARIABLE_ID___Controller
     }
 
 }
 
-// MARK: - Navigation - Public
-
 // MARK: Navigation - Private
+
+extension ___VARIABLE_ID___Coordinator {
+
+    private func navigate(to step: ___VARIABLE_ID___Step) -> StepAction {
+
+    }
+
+}
